a98b0cf6b2d68b0d2e6ea2c1c01fb7c11413f41c
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-05-31 09:44:55 +0530

    day 6

---
+++ backend/schemas/auth.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class RegisterRequest(BaseModel):
+
+    username: str
+
+    email: str
+
+    password: str
+
+
+class LoginRequest(BaseModel):
+
+    username: str
+
+    password: str
+
+
+    
