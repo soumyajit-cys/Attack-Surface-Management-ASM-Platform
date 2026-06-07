6250d97b6c835e9dbacfa6581b143e7a650cb180
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-06-07 23:43:40 +0530

    day 11

---
+++ backend/services/dashboard/dashboard_service.py
@@ -0,0 +1,12 @@
+async def get_dashboard():
+
+    return {
+        "assets": 0,
+        "findings": 0,
+        "critical": 0,
+        "high": 0,
+        "medium": 0,
+        "low": 0
+    }
+
+
