b0e637583166d611796658707fdc7525dcbb74bb
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-08-29 07:28:46 +0530

    feat: remove React import from Layout component to streamline code

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Outlet, NavLink, useLocation } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 import {
